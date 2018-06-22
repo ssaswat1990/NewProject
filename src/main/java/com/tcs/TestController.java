a0254cf4e5ba710197f9217dbc55dfc6a0239219
digambarPa <digambar.patankar@tcs.com>
2018-06-22 17:50:37 +0530

    test changes

--- src/main/java/com/tcs/TestController.java
+++ src/main/java/com/tcs/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 		System.out.println("in controller");
  
-		ModelAndView mv = new ModelAndView("dummyworld1");
+		ModelAndView mv = new ModelAndView("dummyworld2");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
 		return mv;
