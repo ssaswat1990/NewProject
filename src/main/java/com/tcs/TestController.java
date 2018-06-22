96344d6adc4495e94a3dd987505651e699134de1
digambarPa <digambar.patankar@tcs.com>
2018-06-22 16:40:44 +0530

    'v1.0'

---
+++ src/main/java/com/tcs/TestController.java
@@ -0,0 +1,22 @@
+package com.tcs;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+	String message = "Welcome!";
+	 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("dummyworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
